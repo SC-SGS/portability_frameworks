223216bd8f1d1ea946aab350641f278fbb9c02a4
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2025-04-09 15:52:31 +0200

    An example using HPX.

---
+++ HPX/README.md
@@ -0,0 +1,9 @@
+# HPX – High Performance ParallelX
+
+An DAXPY example using the HPX programming framework.
+
+```bash
+cmake -B build .
+cmake --build build
+build/hpx_daxpy
+```
---
+++ HPX/main.cpp
@@ -0,0 +1,37 @@
+#include "hpx/execution.hpp"
+#include "hpx/parallel/segmented_algorithms/for_each.hpp"
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    const int N = 1024;
+
+    // create and fill used data
+    std::vector<double> X(N);
+    std::vector<double> Y(N);
+    for (int i = 0; i < N; ++i) {
+        X[i] = i;
+        Y[i] = 2 * i;
+    }
+    const double alpha = 0.5;
+
+    // create the indices used in the for_each loop
+    std::vector<int> indices(N);
+    std::iota(indices.begin(), indices.end(), 0);
+
+    // the HPX compute kernel
+    hpx::for_each(hpx::execution::par_unseq,
+                  indices.begin(),
+                  indices.end(),
+                  [&](const int idx) {
+                      Y[idx] = alpha * X[idx] + Y[idx];
+                  });
+
+    for (int i = 0; i < 10; ++i) {
+        std::cout << Y[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
